7832a79eabb2c482a3cd3a7659e13da8aa2fd6eb
yongwoo <yongwoowh@naver.com>
2023-06-17 00:29:19 +0900

    백준 ex11659번 (구간 합 구하기) 학습

---
+++ baekjun/src/Array/ex11659.java
@@ -0,0 +1,27 @@
+package Array;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ex11659 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int suNo = Integer.parseInt(st.nextToken());
+		int quizNo = Integer.parseInt(st.nextToken());
+		long[]S = new long [suNo+1];
+		st = new StringTokenizer(br.readLine());
+		for (int i=1; i<=suNo; i ++) {
+			S[i] = S[i-1] + Integer.parseInt(st.nextToken());
+		}
+		for(int q =0; q<quizNo; q++) {
+			st = new StringTokenizer(br.readLine());
+			int i = Integer.parseInt(st.nextToken());
+			int j = Integer.parseInt(st.nextToken());
+			System.out.println(S[j] - S[i-1]);
+		}
+	}
+
+}
