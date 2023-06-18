06bbafd4757768966006650d6739cf8f81196bed
yongwoo <yongwoowh@naver.com>
2023-06-18 19:43:08 +0900

    백준 ex9086 (문자열) 학습 문자형 변수.charAt(x) - x번째 문자 추출 String.valueOf() - 다른 타입을 문자열로 변환

---
+++ baekjun/src/String/ex9086.java
@@ -0,0 +1,17 @@
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ex9086 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int i =0; i<T; i++) {
+			String x =br.readLine();
+			System.out.println(String.valueOf(x.charAt(0))+String.valueOf(x.charAt(x.length()-1))); 
+		}
+	}
+
+}
