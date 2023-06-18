a2bbd8678abbf3fffd02e9078e43eb32186c2a78
yongwoo <yongwoowh@naver.com>
2023-06-18 20:47:22 +0900

    백준 ex10809 (문자열) 학습 String 클래스의 indexOf() - 특정 문자 위치 찾기 StringBuilder 클래스의 append() - 문자열 덧붙이기

--- baekjun/src/String/ex9086.java
+++ baekjun/src/String/ex9086.java
@@ -10,7 +10,8 @@ public class ex9086 {
 		int T = Integer.parseInt(br.readLine());
 		for(int i =0; i<T; i++) {
 			String x =br.readLine();
-			System.out.println(String.valueOf(x.charAt(0))+String.valueOf(x.charAt(x.length()-1))); 
+			System.out.println(String.valueOf(x.charAt(0))+String.valueOf(x.charAt(x.length()-1)));
+			
 		}
 	}
 
